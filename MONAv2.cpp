e2dacf76680c6e1f420f1e3ece1861760acd5b20
joekeo <joekeo@users.noreply.github.com>
2017-07-24 22:32:07 +0100

    Added the read Battery funcion to .ccp
    added a read battery function  to the Librarty, totally based on https://code.google.com/archive/p/tinkerit/wikis/SecretVoltmeter.wiki

--- MONAv2.cpp
+++ MONAv2.cpp
@@ -106,3 +106,15 @@ void MONA::readAllSensors(int16_t *proxSensors){
   proxSensors[4] = analogRead(proxLeft);
 
 }
+//this bit of code was taken from https://code.google.com/archive/p/tinkerit/wikis/SecretVoltmeter.wiki
+ long MONA::readBattery(int16_t battery){
+	 long result; // Read 1.1V reference against AVcc 
+	  ADMUX = _BV(REFS0) | _BV(MUX3) | _BV(MUX2) | _BV(MUX1);
+	  delay(2); // Wait for Vref to settle
+	  ADCSRA |= _BV(ADSC); // Convert
+	  while (bit_is_set(ADCSRA,ADSC));
+	  result = ADCL;
+	  result |= ADCH<<8;
+	  result = 1126400L / result; // Back-calculate AVcc in mV
+	  return result;
+ }
